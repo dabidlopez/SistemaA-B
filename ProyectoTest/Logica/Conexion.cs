8f7d2468629aca234e57f26cfb86b5152f865912
alyso <alyso@ALLYSHON>
2024-08-05 11:16:35 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Server=ALLYSHON\\SQLEXPRESS; Database=DB_PruebTecnica; Trusted_Connection=True; MultipleActiveResultSets=true";
+    }
+}
